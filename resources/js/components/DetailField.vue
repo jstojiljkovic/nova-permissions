32b77eefeff8f2eb072ce5328da8235becdd77b1
JStojiljkovic <jovan.stojiljkovic@5mict.com>
2020-06-29 08:53:14 +0200

    Changed how permissions are shown where there are odd numbers

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,33 +1,33 @@
 <template>
     <panel-item :field="field">
         <p slot="value" class="text-90 flex">
-            <span class="w-full max-col-2" v-if="field.withGroups">
-                <div v-for="(permissions, group) in field.options" :key="group" class="mb-4">
+            <span class="flex flex-wra" v-if="field.withGroups">
+                <div v-for="(permissions, group) in field.options" :key="group" class="w-1/2 mb-2">
                     <h3 class="my-2">{{ __(group) }}</h3>
                     <div v-for="(permission, option) in permissions" :key="option" class="flex-auto">
                         <span
-                        class="inline-block rounded-full w-2 h-2 mr-1"
-                        :class="optionClass(permission.option)"
+                                class="inline-block rounded-full w-2 h-2 mr-1"
+                                :class="optionClass(permission.option)"
                         />
                         <span>{{ permission.label }}</span>
                     </div>
                 </div>
             </span>
-            <span class="w-full max-col-2" v-else>
+            <span class="flex flex-wrap" v-else>
                 <div
-                v-for="(label, option) in field.options"
-                :key="option"
-                class="flex-auto"
+                        v-for="(label, option) in field.options"
+                        :key="option"
+                        class="w-1/2 mb-2"
                 >
                 <span
-                class="inline-block rounded-full w-2 h-2 mr-1"
-                :class="optionClass(option)"
+                        class="inline-block rounded-full w-2 h-2 mr-1"
+                        :class="optionClass(option)"
                 />
                 <span>{{ label }}</span>
             </div>
         </span>
-    </p>
-</panel-item>
+        </p>
+    </panel-item>
 </template>
 
 <script>
@@ -45,16 +45,16 @@
 </script>
 
 <style>
-.max-col-3 {
-    -moz-column-count: 3;
-    -webkit-column-count: 3;
-    column-count: 3;
-    white-space: nowrap;
-}
-.max-col-2 {
-    -moz-column-count: 2;
-    -webkit-column-count: 2;
-    column-count: 2;
-    white-space: nowrap;
-}
+    .max-col-3 {
+        -moz-column-count: 3;
+        -webkit-column-count: 3;
+        column-count: 3;
+        white-space: nowrap;
+    }
+    .max-col-2 {
+        -moz-column-count: 2;
+        -webkit-column-count: 2;
+        column-count: 2;
+        white-space: nowrap;
+    }
 </style>
